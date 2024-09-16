d7d3db20f7a36905eafdae62595878fc70c6cc15
GasanaJr <gasanadj2003@gmail.com>
2024-09-16 11:30:30 +0200

    feat(scene): Create obstacle game scene and player movements

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xVal = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+        float zVal = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        transform.Translate(xVal, 0, zVal);
+    }
+}
